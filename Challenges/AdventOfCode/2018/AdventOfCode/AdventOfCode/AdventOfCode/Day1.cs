8d8fa8e6ead8094001f6b5547242970b37dc67db
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2018-12-31 17:51:26 +0000

    fix issues with tests

--- Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/AdventOfCode/Day1.cs
+++ Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/AdventOfCode/Day1.cs
@@ -1,4 +1,4 @@
-using System.Runtime.InteropServices.ComTypes;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace AdventOfCode
@@ -11,7 +11,7 @@ namespace AdventOfCode
         }
 
         [Test]
-        public void TestDay1()
+        public void TestDay1Part1()
         {
             //Load file
             string[] lines = System.IO.File.ReadAllLines(@"C:\temp\repo\CodeChallenges\Challenges\AdventOfCode\2018\Day 1\data.txt");
@@ -33,6 +33,58 @@ namespace AdventOfCode
                 frequency += freq;
             }
             Assert.AreEqual(frequency, 502);
+        }
+
+        [Test]
+        public void TestDay1Part2()
+        {
+            //Load file
+            string[] lines = System.IO.File.ReadAllLines(@"C:\temp\repo\CodeChallenges\Challenges\AdventOfCode\2018\Day 1\data.txt");
+            List<int> frequencies = new List<int>();
+            int frequency = 0;
+            foreach (var line in lines)
+            {
+                int freq = 0;
+                if (line.Contains("-"))
+                {
+                    string number = line.Replace("-", "");
+                    freq = int.Parse(number);
+                    freq = freq * -1;
+                }
+                else if (line.Contains("+"))
+                {
+                    string number = line.Replace("+", "");
+                    freq = int.Parse(number);
+                }
+
+                frequencies.Add(freq);
+            }
+
+            //Now we have a list of frequencies.
+            Dictionary<int, bool> frequencyReached = new Dictionary<int, bool>();
+            bool found = false;
+            do
+            {
+                foreach (var item in frequencies)
+                {
+                    frequency += item;
+                    bool value;
+                    if (!frequencyReached.TryGetValue(frequency, out value))
+                    {
+                        frequencyReached.Add(frequency, true);
+                    }
+                    else
+                    {
+                        //Frequency found twice
+                        //exit loop
+                        found = true;
+                        break;
+                    }
+                }
+            } while (!found);
+
+
+            Assert.AreEqual(frequency, 71961);
         }
     }
 }
--- Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/AdventOfCode/Day2.cs
+++ Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/AdventOfCode/Day2.cs
@@ -12,30 +12,6 @@ namespace AdventOfCode
         {
         }
 
-        [Test]
-        public void TestDay2()
-        {
-            //Load file
-            string[] lines = System.IO.File.ReadAllLines(@"C:\temp\repo\CodeChallenges\Challenges\AdventOfCode\2018\Day 1\data.txt");
-            int frequency = 0;
-            foreach (var line in lines)
-            {
-                int freq = 0;
-                if (line.Contains("-"))
-                {
-                    string number = line.Replace("-", "");
-                    freq = int.Parse(number);
-                    freq = freq * -1;
-                }
-                else if (line.Contains("+"))
-                {
-                    string number = line.Replace("+", "");
-                    freq = int.Parse(number);
-                }
-
-                frequency += freq;
-            }
-            Assert.AreEqual(frequency, 502);
-        }
+        
     }
 }
