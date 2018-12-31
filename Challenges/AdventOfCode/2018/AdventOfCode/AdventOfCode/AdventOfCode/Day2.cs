56a2af238d737a4736cfcdb8a47bf14b77c5dbb0
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2018-12-31 20:17:19 +0000

    adding second day

--- Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/AdventOfCode/Day2.cs
+++ Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/AdventOfCode/Day2.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace AdventOfCode
 {
@@ -12,7 +13,37 @@ namespace AdventOfCode
         [Test]
         public void TestDay2()
         {
+            //Load file
+            string[] lines = System.IO.File.ReadAllLines(@"C:\temp\repo\CodeChallenges\Challenges\AdventOfCode\2018\Day 2\data.txt");
+            int counttwo = 0;
+            int countthree = 0;
+            foreach (var text in lines)
+            {
+                Dictionary<char, int> repetition = new Dictionary<char, int>();
+                foreach (var word in text)
+                {
+                    int value;
+                    if (!repetition.TryGetValue(word, out value))
+                    {
+                        repetition.Add(word, 1);
+                    }
+                    else
+                    {
+                        repetition[word]++;
+                    }
+                }
 
+                foreach (var item in repetition)
+                {
+                    if (item.Value == 2)
+                        counttwo++;
+                    if (item.Value == 3)
+                        countthree++;
+                }
+
+            }
+
+            Assert.AreEqual(countthree*counttwo, 18357);
         }
     }
 }
