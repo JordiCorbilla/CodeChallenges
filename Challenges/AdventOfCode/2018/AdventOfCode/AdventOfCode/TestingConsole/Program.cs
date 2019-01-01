7d7752ef1dfc0230d0cc3610f3d99ab3ca1594db
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2019-01-01 22:56:24 +0000

    adding initial test

--- Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/TestingConsole/Program.cs
+++ Challenges/AdventOfCode/2018/AdventOfCode/AdventOfCode/TestingConsole/Program.cs
@@ -6,7 +6,24 @@ namespace TestingConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[][] matrix = new char[1000][];
+            for (int i = 0; i < 1000; i++)
+            {
+                matrix[i] = new char[1000];
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                string row = "";
+                for (int j = 0; j < 1000; j++)
+                {
+                    matrix[i][j] = '.';
+                    row += matrix[i][j];
+                }
+                Console.WriteLine(row);
+            }
+
+            Console.ReadKey();
         }
     }
 }
